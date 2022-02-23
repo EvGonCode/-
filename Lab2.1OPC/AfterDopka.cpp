6f3dc80bea3cd450c5005d223ce95366c85348b3
Honcharov Yavhen <40567209+EvGonCode@users.noreply.github.com>
2022-02-23 23:57:58 +0300

    Add files via upload

---
+++ Lab2.1OPC/AfterDopka.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "Header.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+
+    string mainFile = "myFile.txt";
+    string pairFile = "fileOfPairStrings.txt";
+    string nonPairFile = "fileOfNonPairStrings.txt";
+    int n;
+    n = requestN();
+    liveWriteInNewFile(mainFile);
+    copyPairStrings(mainFile, pairFile);
+    copyNonPairStrings(mainFile, nonPairFile);
+
+    cout << "Головний файл" << endl;
+    printFile(mainFile);
+
+    cout << "Файл з парними рядками" << endl;
+    printFile(pairFile);
+
+    cout << "Файл з непарними рядками" << endl;
+    printFile(nonPairFile);
+
+    sortFileByAlphabet(pairFile);
+    sortFirstNStringsOfWordsByAlphabet(nonPairFile,n);
+
+    cout << "Файл з парними рядками відсортований" << endl;
+    printFile(pairFile);
+
+    cout << endl;
+
+    cout << "Файл з непарними рядками відсортований" << endl;
+    printFile(nonPairFile);
+}
